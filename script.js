cffb04930638f9776ab4ff3f4616c1b23b852007
Ponce <ponceluis974@gmail.com>
2023-04-21 17:41:37 -0700

    added form

--- script.js
+++ script.js
@@ -13,6 +13,11 @@
 //  const longitude = position.coords.longitude;
 //  document.getElementById("location").innerHTML = `Latitude: ${latitude}, Longitude: ${longitude}`;
 //}
+const locationElement = document.querySelector('#location');
+const tempElement = document.querySelector('#temperature');
+const windElement = document.querySelector('#wind');
+const humidElement = document.querySelector('#humidity');
+const rainElement = document.querySelector('#rain');
 
 window.addEventListener('load', () => {
   if (navigator.geolocation) {
@@ -25,24 +30,71 @@ window.addEventListener('load', () => {
 function showPosition(position) {
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
+  //const locationElement = document.querySelector('#location');
   console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
 
+  url1= `http://localhost:3000/initial/location?lat=${latitude}&lon=${longitude}`
+  fetch(url1)
+  .then(response => response.json())
+  .then(data => {
+    console.log('helo')
+    locationElement.textContent = `${data.name}`;
+    tempElement.textContent = `${data.main.temp}`;
+    windElement.textContent = `${data.wind.speed}`;
+    humidElement.textContent = `${data.main.humidity}`;
+    
+    if(data.weather[0].main === 'Rain' || data.weather[0].main === 'Thunderstorm' || data.weather[0].main === 'Drizzle'){
+      const rainvolume = data.rain['1h'] || data.rain['3h'];
+      rainElement.textContent = `${rainvolume}`;
+      
+      const iconElement = document.getElementById("icon");
+      iconElement.setAttribute("src", "./icons/storm.png");
+    }
+    else{
+      console.log('it is not currently raining');
+     
+    }
+    if(data.weather[0].main === 'Snow' ){
+      const snowvolume = data.snow['1h'] || data.snow['3h'];
+      rain=Element.textContent = `${snowvolume}`;
+      
+      const iconElement = document.getElementById("icon");
+      iconElement.setAttribute("src", "./icons/snow.png");
+    }
+    else{
+      console.log('it is not currently snowing');
+      
+    }
+    if(data.weather[0].main === 'Clouds' ){
+      const iconElement = document.getElementById("icon");
+      iconElement.setAttribute("src", "./icons/cloudy.png");
+    }
+    if(data.weather[0].main === 'Clear'){
+      const iconElement = document.getElementById("icon");
+      iconElement.setAttribute("src", "./icons/sun.png");
+    }
+  
+
+  })
+  .catch(error => {
+    console.error(error)})
+
   // Display the user's location on the page
-  const locationElement = document.querySelector('#location');
-  locationElement.textContent = `Latitude: ${latitude}, Longitude: ${longitude}`;
+  
+  //locationElement.textContent = `Latitude: ${latitude}, Longitude: ${longitude}`;
 }
 
 const submitBtn = document.querySelector('#search-button');
-submitBtn.addEventListener('click', (event) => {
+function handleSubmit(event) {
   event.preventDefault();
-  const inputData = document.querySelector('#name').value 
+  const inputData = event.target.elements.cityname.value 
 
   const url = `http://localhost:3000/weather/${inputData}`
-  const locationElement = document.querySelector('#location');
-  const tempElement = document.querySelector('#temperature');
-  const windElement = document.querySelector('#wind');
-  const humidElement = document.querySelector('#humidity');
-  const rainElement = document.querySelector('#rain');
+  //const locationElement = document.querySelector('#location');
+  //const tempElement = document.querySelector('#temperature');
+  //const windElement = document.querySelector('#wind');
+  //const humidElement = document.querySelector('#humidity');
+  //const rainElement = document.querySelector('#rain');
   
   
   
@@ -90,9 +142,9 @@ submitBtn.addEventListener('click', (event) => {
   
 
   })
-  .catch(error => console.error(error))
+  .catch(error => {console.error(error)})
   
 
   
   
-})
+}
